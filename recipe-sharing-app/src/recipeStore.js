b778af93aa865268024f88f34482bd4fc8e58bf1
ALI Khaldoun <ali.khaldoun03@gmail.com>
2024-08-25 07:20:46 +0100

    update files

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -1,9 +1,9 @@
 import create from 'zustand';
 
-const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+const useRecipeStore = create((set) => ({
+  recipes: [], // Initial state
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }), // Function to update recipes
+  addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
 }));
 
 export default useRecipeStore;

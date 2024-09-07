149a66df16113837e9d786459e48a0588a13a48d
ALI Khaldoun <ali.khaldoun03@gmail.com>
2024-09-07 20:13:49 +0100

    update file

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
 function Profile() {
   return (
     <div>
       <h2>Profile</h2>
       <nav>
-        <Link to="details">Profile Details</Link> | {' '}
-        <Link to="settings">Profile Settings</Link>
+        <a href="/profile/details">Profile Details</a> | {' '}
+        <a href="/profile/settings">Profile Settings</a>
       </nav>
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
       <Outlet />
     </div>
   );

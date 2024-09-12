4f4fd1c31a3ae950d86eb03ad90d1cba8d124550
ALI Khaldoun <ali.khaldoun03@gmail.com>
2024-09-12 19:59:22 +0100

    update file

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  content: ['./src/*/.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/*/.{js,jsx,ts,tsx}', './public/index.html'], // For compatibility with Tailwind 2.x
+  content: ['./src/*/.{js,jsx,ts,tsx}', './public/index.html'], // For Tailwind 3.x
   theme: {
     extend: {},
   },
